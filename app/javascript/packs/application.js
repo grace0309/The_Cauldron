b971737321b844fa1478fb30073c636fc3f9ab3d
mayong <hhmayong@gmail.com>
2019-08-27 15:24:44 +0800

    started JS and edited navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,11 @@
 import "bootstrap";
 console.log('HELLOOOOO')
+const input = document.querySelector('.form-inputs');
+console.log(input)
+input.addEventListener('change', (event) => {
+  const startDate = document.getElementById('booking_start_date');
+  const endDate = document.getElementById('booking_end_date');
+  console.log(startDate)
+  console.log(endDate)
+});
+
