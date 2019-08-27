2511579c35f3e78c751a0b0fb55d53adbcd1162f
meixuanoh <ohmx93@gmail.com>
2019-08-27 15:24:33 +0800

    authorization for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,31 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_post, only: [:show, :destroy, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
+    authorize @post
   end
 
   def new
     @post = Post.new
+    authorize @post
   end
 
   def create
     @post = Post.new(post_params)
+    authorize @post
     @post.category_id = params[:post][:category_id]
     @post.user_id = current_user.id
     @post.save
     if @post.save
       redirect_to post_path(@post)
     else
-      render "new"
+      render :new
     end
-    @Posts = Post.all
   end
 
   def edit
@@ -36,7 +40,6 @@ class PostsController < ApplicationController
     @post.destroy
     @posts = Post.all
   end
-
 
   private
 
