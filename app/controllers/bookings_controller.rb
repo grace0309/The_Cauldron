83738478b625bc3b4d84d862498fdfc4e204b351
mayong <hhmayong@gmail.com>
2019-08-27 14:23:57 +0800

    bookings controller and view done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @booking = Booking.new(booking_params)
+    @booking.post = @post
+    @booking.user_id = current_user.id
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
+  end
 end
