0ed4d67af61fca74cf2bbd3c7f0d740bba664ff9
mayong <hhmayong@gmail.com>
2019-08-28 13:24:27 +0800

    booking card done

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [ :create, :new]
    end
 
-  resources :bookings, only: [ :edit, :update, :destroy]
+  resources :bookings, only: [ :show, :edit, :update, :destroy]
   resources :reviews, only: [ :edit, :update, :destroy]
 
   get "/dashboard", to: "pages#dashboard", as: :dashboard
