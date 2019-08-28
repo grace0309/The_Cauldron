8246fcb1b67fbd009f9a89fbb27561a8d4c3b1d4
Grace Cheung <cgrace0309@gmail.com>
2019-08-28 14:56:46 +0800

    flatpicker added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -12,4 +12,11 @@ class Booking < ApplicationRecord
     daydiff = ((enddate - startdate) / 86400) + 1
     self.total_price = daydiff * self.post.price
   end
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,10 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
